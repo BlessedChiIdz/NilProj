a5d2e7ee9917ff2be5e760385818263c5bdeb3e2
rik51 <animeboy3228@outlook.com>
2024-01-31 22:24:48 +0700

    Change Header margin

---
+++ src/Components/LinkPageMainPart.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LinkPageMainPart = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default LinkPageMainPart;
--- src/Pages/LinksPage.tsx
+++ src/Pages/LinksPage.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from "../Components/Header";
 import TopLinks from "../Components/TopLinks";
+import LinkPageMainPart from "../Components/LinkPageMainPart";
 
 const LinksPage = () => {
     return (
         <div>
             <Header/>
             <TopLinks/>
+            <LinkPageMainPart/>
         </div>
     );
 };
